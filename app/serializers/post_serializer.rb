20c282ef5b4953b14e0a8b0ec2944afec9904204
github username <chang.lim92@gmail.com>
2018-08-21 15:25:09 -0400

    styles/:id & posts/:id set up

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,9 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  # attributes :id, :name, :picture_url, :likes, :location, :articles
+  attributes :id, :name, :picture_url, :likes, :location, :articles
   has_many :post_styles, embed: :objects
   has_many :styles, through: :post_styles
   belongs_to :user
-  
+
   def include_tags?
     object.association(:styles).loaded?
   end
--- app/serializers/post_style_serializer.rb
+++ app/serializers/post_style_serializer.rb
@@ -1,5 +1,5 @@
 class PostStyleSerializer < ActiveModel::Serializer
-  # attributes :id
+  attributes :id
   belongs_to :post, foreign_key: :post_id
   belongs_to :style, foreign_key: :style_id
 end
