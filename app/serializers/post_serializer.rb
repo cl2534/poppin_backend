0cec0350118a3f2cdea5457c532b104d94b4a674
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-08-21 17:04:31 -0400

    added user_id to postserializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :name, :picture_url, :likes, :location, :articles
+  attributes :id, :name, :picture_url, :likes, :location, :articles, :user_id
   has_many :post_styles, embed: :objects
   has_many :styles, through: :post_styles
   belongs_to :user
